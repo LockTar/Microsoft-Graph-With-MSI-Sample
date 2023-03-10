99525b626010eb6e773965a2219a9c3e52d303f0
Ralph Jansen <ralph.jansen@outlook.com>
2023-03-10 17:58:41 +0100

    Move to own file

---
+++ Core/Helpers/TokenProvider.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Identity;
+using Microsoft.Kiota.Abstractions.Authentication;
+
+namespace Core.Helpers
+{
+    public class TokenProvider : IAccessTokenProvider
+    {
+        private readonly DefaultAzureCredential _credential;
+
+        public TokenProvider(DefaultAzureCredential credential)
+        {
+            _credential = credential;
+        }
+
+        public async Task<string> GetAuthorizationTokenAsync(
+            Uri uri,
+            Dictionary<string, object> additionalAuthenticationContext = default,
+            CancellationToken cancellationToken = default)
+        {
+            var tokenResult = await _credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new string[] { "https://graph.microsoft.com" }));
+
+            var token = tokenResult.Token;
+
+            return token;
+        }
+
+        public AllowedHostsValidator AllowedHostsValidator { get; }
+    }
+}
