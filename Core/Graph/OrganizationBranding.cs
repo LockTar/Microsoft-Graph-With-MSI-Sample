7da09d0b79a756d312617dd75da9c6bb3f4f6507
Ralph Jansen <ralph.jansen@outlook.com>
2023-03-10 12:51:48 +0100

    First branding

---
+++ Core/Graph/OrganizationBranding.cs
@@ -0,0 +1,24 @@
+﻿using Core.Helpers;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Models.ODataErrors;
+
+namespace Core.Graph
+{
+    public static class OrganizationBranding
+    {
+        public static async Task DisplayBrandingAsync(GraphServiceClient graphClient, bool writeJsonObjectsToOutput = true)
+        {
+            var result = await graphClient.Organization
+                .GetAsync(requestConfig =>
+                    requestConfig.QueryParameters.Expand = new string[] { "branding" });
+
+            if (writeJsonObjectsToOutput)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Branding in JSON:");
+                Console.WriteLine(result.ToFormattedJson());
+            }
+        }
+    }
+}
