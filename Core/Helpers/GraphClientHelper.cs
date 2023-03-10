114f60b170b2587c080d4ae92b8f5e0dab93cb56
Ralph Jansen <ralph.jansen@outlook.com>
2023-03-10 18:14:50 +0100

    Add clientcredential helpers for beta

--- Core/Helpers/GraphClientHelper.cs
+++ Core/Helpers/GraphClientHelper.cs
@@ -1,12 +1,21 @@
-﻿using Azure.Identity;
+﻿using Azure.Core;
+using Azure.Identity;
+using Core.Logging;
 using Microsoft.Graph;
+using Microsoft.Graph.Authentication;
 using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary.Middleware;
+using System.Net.Http;
 
 namespace Core.Helpers
 {
     public static class GraphClientHelper
     {
-        public static async Task<GraphServiceClient> InitializeGraphClientWithMsiAsync()
+        private const string TenantId = "";
+        private const string ClientId = "";
+        private const string ClientSecret = "";
+
+        public static async Task<GraphServiceClient> InitializeGraphClientWithMsiAsync(bool enableHttpRequestHandler)
         {
             var credential = new DefaultAzureCredential();
             var tokenResult = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new string[] { "https://graph.microsoft.com" }));
@@ -17,37 +26,30 @@ namespace Core.Helpers
             Console.WriteLine("\n\n");
 
             TokenProvider accessTokenProvider = new TokenProvider(credential);
-            var authenticationProvider = new BaseBearerTokenAuthenticationProvider(accessTokenProvider);
-            var graphServiceClient = new GraphServiceClient(authenticationProvider);
+            var authProvider = new BaseBearerTokenAuthenticationProvider(accessTokenProvider);
 
-            //var graphServiceClient = new GraphServiceClient(
-            //    new DelegateAuthenticationProvider((requestMessage) =>
-            //    {
-            //        requestMessage
-            //            .Headers
-            //            .Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpClient httpClient = GetHttpClientWithDefaultAndCustomHandlers(enableHttpRequestHandler);
 
-            //        return Task.CompletedTask;
-            //    }));
+            var graphClient = new GraphServiceClient(httpClient, authProvider);
 
-            return graphServiceClient;
+            return graphClient;
         }
 
-        public static async Task<GraphServiceClient> InitializeGraphClientWithClientCredentialsAsync()
+        private static void SetupClientSecretCredentialAndScopes(out string[] scopes, out ClientSecretCredential clientSecretCredential)
         {
             // The client credentials flow requires that you request the
             // /.default scope, and preconfigure your permissions on the
             // app registration in Azure. An administrator must grant consent
             // to those permissions beforehand.
-            var scopes = new[] { "https://graph.microsoft.com/.default" };
+            scopes = new[] { "https://graph.microsoft.com/.default" };
 
             // Multi-tenant apps can use "common",
             // single-tenant apps must use the tenant ID from the Azure portal
-            var tenantId = "";
+            var tenantId = TenantId;
 
             // Values from app registration
-            var clientId = "";
-            var clientSecret = "";
+            var clientId = ClientId;
+            var clientSecret = ClientSecret;
 
             // using Azure.Identity;
             var options = new TokenCredentialOptions
@@ -56,36 +58,65 @@ namespace Core.Helpers
             };
 
             // https://learn.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
-            var clientSecretCredential = new ClientSecretCredential(
+            clientSecretCredential = new ClientSecretCredential(
                 tenantId, clientId, clientSecret, options);
+        }
 
-            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+        public static async Task<GraphServiceClient> InitializeGraphClientWithClientCredentialsAsync(bool enableHttpRequestHandler)
+        {
+            string[] scopes;
+            ClientSecretCredential clientSecretCredential;
+            SetupClientSecretCredentialAndScopes(out scopes, out clientSecretCredential);
+
+            var authProvider = new AzureIdentityAuthenticationProvider(clientSecretCredential, scopes);
+
+            HttpClient httpClient = GetHttpClientWithDefaultAndCustomHandlers(enableHttpRequestHandler);
+
+            var graphClient = new GraphServiceClient(httpClient, authProvider);
+
+            //var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
 
             return graphClient;
         }
-    }
 
-    public class TokenProvider : IAccessTokenProvider
-    {
-        private readonly DefaultAzureCredential _credential;
-
-        public TokenProvider(DefaultAzureCredential credential)
+        public static async Task<Microsoft.Graph.Beta.GraphServiceClient> InitializeGraphBetaClientWithClientCredentialsAsync(bool enableHttpRequestHandler)
         {
-            _credential = credential;
+            string[] scopes;
+            ClientSecretCredential clientSecretCredential;
+            SetupClientSecretCredentialAndScopes(out scopes, out clientSecretCredential);
+
+            var authProvider = new AzureIdentityAuthenticationProvider(clientSecretCredential, scopes);
+
+            HttpClient httpClient = GetHttpClientWithDefaultAndCustomHandlers(enableHttpRequestHandler);
+
+            var graphClient = new Microsoft.Graph.Beta.GraphServiceClient(httpClient, authProvider);
+
+            //var graphClient = new Microsoft.Graph.Beta.GraphServiceClient(clientSecretCredential, scopes);
+
+            return graphClient;
         }
 
-        public async Task<string> GetAuthorizationTokenAsync(
-            Uri uri, 
-            Dictionary<string, object> additionalAuthenticationContext = default,
-            CancellationToken cancellationToken = default)
-        {            
-            var tokenResult = await _credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new string[] { "https://graph.microsoft.com" }));
+        private static HttpClient GetHttpClientWithDefaultAndCustomHandlers(bool enableHttpRequestHandler)
+        {
+            var handlers = GraphClientFactory.CreateDefaultHandlers();
 
-            var token = tokenResult.Token;
+            // Sample code to remove a default handler
+            ////var compressionHandler = handlers.Where(h => h is CompressionHandler).FirstOrDefault();
+            ////handlers.Remove(compressionHandler);
 
-            return token;
+            if (enableHttpRequestHandler)
+            {
+                // Add logging handler before the compression handler. Compression handler is created by CreateDefaultHandlers()
+                HttpRequestMessageLoggingHandler loggingHandler = new();
+                handlers.Insert(0, loggingHandler);
+            }
+
+            // Add a new one
+            // ChaosHandler simulates random server failures
+            ////handlers.Add(new ChaosHandler());
+
+            var httpClient = GraphClientFactory.Create(handlers);
+            return httpClient;
         }
-
-        public AllowedHostsValidator AllowedHostsValidator { get; }
     }
 }
