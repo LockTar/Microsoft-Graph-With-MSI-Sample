a358ceddbfac4b9b327d06fb082daa233d34fe03
Ralph Jansen <ralph.jansen@outlook.com>
2023-03-13 11:34:39 +0100

    Update branding of tenant

--- Core/Graph/OrganizationBranding.cs
+++ Core/Graph/OrganizationBranding.cs
@@ -1,18 +1,18 @@
 ﻿using Core.Helpers;
 using Microsoft.Graph;
+using Microsoft.Graph.Models;
 
 namespace Core.Graph
 {
     public static class OrganizationBranding
     {
-        public static async Task DisplayBrandingAsync(GraphServiceClient graphClient, bool writeJsonObjectsToOutput = true)
+        public static async Task DisplayBrandingAsync(GraphServiceClient graphClient, string organizationId, bool writeJsonObjectsToOutput = true)
         {
-            var result = await graphClient.Organization["f6f46aaf-ecdb-47b2-a816-cdc942b1b411"]
+            var result = await graphClient.Organization[organizationId]
                 .Branding
                 .GetAsync(requestConfig =>
                 {
-                    //requestConfig.QueryParameters.Select = new string[] { "branding" };
-                    //requestConfig.QueryParameters.Expand = new string[] { "branding" };
+                    requestConfig.QueryParameters.Select = new string[] { "BackgroundColor" }; // doesn't work (yet)???
                 });
 
             if (writeJsonObjectsToOutput)
@@ -20,6 +20,32 @@ namespace Core.Graph
                 Console.WriteLine();
                 Console.WriteLine("Branding in JSON:");
                 Console.WriteLine(result.ToFormattedJson());
+            }
+        }
+
+        public static async Task UpdateBrandingAsync(GraphServiceClient graphClient, string organizationId, bool writeJsonObjectsToOutput = true)
+        {
+            var branding = new OrganizationalBranding();
+            var seconds = DateTime.UtcNow.Second;
+
+            if (seconds >= 0 && seconds <= 30)
+            {
+                branding.BackgroundColor = "#99CCFF";
+            }
+            else
+            {
+                branding.BackgroundColor = "#2969E8";
+            }
+
+            await graphClient.Organization[organizationId]
+                .Branding
+                .PatchAsync(branding);
+
+            if (writeJsonObjectsToOutput)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Changed branding to:");
+                await DisplayBrandingAsync(graphClient, organizationId, writeJsonObjectsToOutput);
             }
         }
     }
